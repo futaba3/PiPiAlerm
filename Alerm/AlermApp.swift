5a1cb4c250647b78906ecc7267ca6ba046d2c4d2
Futaba3 <nyokinyoki41aya@gmail.com>
2021-03-30 22:03:17 +0900

    Initial Commit

---
+++ Alerm/AlermApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlermApp.swift
+//  Alerm
+//
+//  Created by 工藤彩名 on 2021/03/30.
+//
+
+import SwiftUI
+
+@main
+struct AlermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
